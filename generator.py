3443b1d2640417809769732c2734e69fee2e17d4
Mateamihaljeviic <matea.mihaljevic@fpmoz.sum.ba>
2024-09-26 16:03:10 +0200

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,22 @@
+'''
+Napraviti generator funkcije za ispis svih parnih i svih
+neparnih brojeva manjih od prosljeđenog parametra.
+'''
+
+import random
+
+def parni(broj):
+    for b in range(broj):
+        if b%2==0:
+            yield b
+
+def neparni(broj):
+    for b in range(broj):
+        if b%2!=0:
+            yield b
+
+broj=random.randint(0, 100)
+print(broj)
+
+print("Parni: ", list(parni(broj)))
+print("Neparni: ", list(neparni(broj)))
